e6511aaf62bf6b7524d76c798ca21d1ed68d775d
PolinaKuznecova87 <poly.voinylenko@gmail.com>
2023-02-08 16:39:09 +0300

    add test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,20 +8,38 @@ public class CashbackHackServiceTest {
 
     @Test
     public void testRemain() {
-       int amount = 250;
-       int expected = 750;
+
         CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(amount);
-        assertEquals(expected,actual);
+        assertEquals(service.remain(250),750);
 
     }
     @Test
     public void testRemainMin() {
-        int amount = 3000;
-        int expected = 1000;
         CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(amount);
-        assertEquals(expected,actual);
+
+        assertEquals(service.remain(1000),0);
 
     }
+    @Test
+    public void testRemainBelowThousand() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(999),1);
+
+    }
+    @Test
+    public void testRemainOne() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(1),999);
+
+    }
+    @Test
+    public void testRemainZiro() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(0),1000);
+
+    }
+
 }
